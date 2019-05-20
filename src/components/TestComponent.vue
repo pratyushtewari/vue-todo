0828820f11e1fe7b5617800d80082f41fb4d1311
Pratyush Tewari <tewari.pratyush@gmail.com>
2019-05-20 01:11:44 -0400

    Added the first template with router configuration for 2 pages

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="logo" src="../assets/logo.svg">
+    <TestComponent msg="This is a test message"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TestComponent from '@/components/TestComponent.vue'
+
+export default {
+  name: 'home',
+  components: {
+    TestComponent
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 100px;
+}
+</style>
+
